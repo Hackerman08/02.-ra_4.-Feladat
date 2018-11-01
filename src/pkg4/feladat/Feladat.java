3854ce0c80bd17be0a913d688fd57ab5991464d2
Szendrényi Ákos <Szendrnyi kos@DESKTOP-SG6VCUU>
2018-11-01 15:21:44 +0100

    Kész

--- src/pkg4/feladat/Feladat.java
+++ src/pkg4/feladat/Feladat.java
@@ -1,12 +1,38 @@
 
 package pkg4.feladat;
 
+import java.util.Scanner;
+
 
 public class Feladat {
 
     
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int a;
+        int b;
+        int c;
+        int terület;
+        
+       System.out.println("Kérem adja meg az A oldalt: ");
        
+        a= input.nextInt();
+        System.out.println("Kérem adja meg a B oldalt: ");
+       
+        b= input.nextInt();
+        System.out.println("Kérem adja meg a C oldalt: ");
+       
+        c= input.nextInt();
+        terület=a+b+c;
+      
+        if(a*a+b*b==c*c){
+            System.out.println("A hárómszőg területe: "+terület );
+        }else{
+            System.out.println("Hibás adatok" );
+        }
+      
     }
     
 }
